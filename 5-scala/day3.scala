d4b7d52d9dd76f55edff9ac59f8936d7b478d1ed
Pedro Rijo <pedrorijo91@gmail.com>
2015-02-25 21:49:34 +0000

    added day 3 Scala file but without implementation due to actors API deprecated

---
+++ 5-scala/day3.scala
@@ -0,0 +1,5 @@
+//actors API deprecated
+/*
+instead of calculate the size of each page should get page body and look for '(http(s?)://)?www.\w+' or similar
+after find links, open and calculate size
+ */
